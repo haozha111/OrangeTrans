ccd7d870e06ffd5f2469bc5c34d0fbe4041de1e6
A30041839 <A30041839@gmail.com>
2014-04-27 16:23:30 +0800

    bug fixes

--- OrangeTrans/main.cpp
+++ OrangeTrans/main.cpp
@@ -21,10 +21,11 @@ using namespace std;
 
 void main(int argc, char* argv[])
 {
-	using namespace OrangeTraining;
-	string src = "C:\\Users\\Administrator\\Desktop\\orange\\training data\\chinese.txt";
-	string tgt = "C:\\Users\\Administrator\\Desktop\\orange\\training data\\english.txt";
-	string align = "C:\\Users\\Administrator\\Desktop\\orange\\training data\\Alignment.txt";
+    using namespace OrangeTraining;
+
+	string src = "C:\\Users\\v-haozha\\Desktop\\orange\\training data\\chinese.txt"; 
+	string tgt = "C:\\Users\\v-haozha\\Desktop\\orange\\training data\\english.txt";
+	string align = "C:\\Users\\v-haozha\\Desktop\\orange\\training data\\Alignment.txt";
 	string rule = "C:\\Users\\Administrator\\Desktop\\orange\\work\\phrase extraction\\rule.txt";
 	string ruleinv = "C:\\Users\\Administrator\\Desktop\\orange\\work\\phrase extraction\\ruleinv.txt";
 	/*PhraseExtractionPipeline p = PhraseExtractionPipeline(src, tgt, align, rule, ruleinv);
@@ -32,8 +33,8 @@ void main(int argc, char* argv[])
 
 
 	/*******************************/
-	string s2t = "C:\\Users\\Administrator\\Desktop\\orange\\work\\phrase extraction\\s2t.txt";
-	string t2s = "C:\\Users\\Administrator\\Desktop\\orange\\work\\phrase extraction\\t2s.txt";
+	string s2t = "C:\\Users\\v-haozha\\Desktop\\orange\\work\\phrase extraction\\s2t.txt";
+	string t2s = "C:\\Users\\v-haozha\\Desktop\\orange\\work\\phrase extraction\\t2s.txt";
 
 	LexicalTransPipeline lexpipe;
 	lexpipe.Start(src, tgt, align, s2t, t2s);
--- OrangeTrans/phrase extraction/LexicalTranslation.h
+++ OrangeTrans/phrase extraction/LexicalTranslation.h
@@ -24,19 +24,21 @@ using namespace std;
 
 namespace OrangeTraining
 {
-	class LexicalTranslation
-	{
-	public:
-		LexicalTranslation();
-		void BuildLexicon(string &psource, string &ptarget
-			, string &palign);
-		void Write(string &ps2t, string &pt2s);
-	    
-	private:
-		map<string, map<string, size_t> > m_globalCooccur; //!record the cooccur statistics of source word and target word
-		map<string, size_t> m_srccount; //!record the total count of source word
-		map<string, size_t> m_tgtcount; //!record the total count of target word
-	};
+    class LexicalTranslation
+    {
+    public:
+        LexicalTranslation();
+        void BuildLexicon(string &psource, string &ptarget
+            , string &palign);
+        void Write(string &ps2t, string &pt2s);
+
+    private:
+        map<string, map<string, size_t> > m_globalCooccur; //!record the cooccur statistics of source word and target word
+        map<string, size_t> m_srccount; //!record the total count of source word
+        map<string, size_t> m_tgtcount; //!record the total count of target word
+        map<string, map<string, double> > m_src2tgtlex; //!source->target lexicon
+        map<string, map<string, double> > m_tgt2srclex; //!target->source lexicon
+    };
 }
 
 #endif
